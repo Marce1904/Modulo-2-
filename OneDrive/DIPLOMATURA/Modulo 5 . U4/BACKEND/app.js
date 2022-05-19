e2e6c66717ba2a0ddc521c91f8c00bfa34cfc081
Marce1904 <95431496+Marce1904@users.noreply.github.com>
2022-05-19 08:04:08 -0300

    Subimos todo en conjunto backend-frontend

--- OneDrive/DIPLOMATURA/Modulo 5 . U4/BACKEND/app.js
+++ OneDrive/DIPLOMATURA/Modulo 5 . U4/BACKEND/app.js
@@ -3,15 +3,25 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var fileUpload = require('express-fileupload');
+var cors = require('cors');
+
+
 
 require('dotenv').config();
+var session = require ('express-session');
+
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var loginRouter = require("./routes/admin/login");
+var adminRouter = require ("./routes/admin/novedades");
+var apiRouter= require("./routes/api");
 
 
 var app = express();
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,9 +33,41 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(session({
+  secret: 'gatogato',
+  cookie: {maxAge: null},
+  resave: false,
+  saveUninitialized: true
+
+}))
+
+secured = async (req, res, next) => {
+  try{
+    console.log(req.session.id_usuario);
+    if(req.session.id_usuario) {
+      next();
+    } else {
+      res.redirect ('/admin/login')
+    }
+  } catch (error) {
+    console.log (error);
+  }
+}
+
+
+app.use(fileUpload({
+  useTempFiles: true,
+  tempFileDir: '/tmp/'
+}));
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/admin/login', loginRouter);
+app.use('/admin/novedades', secured, adminRouter);
+app.use('/api' , cors(), apiRouter);
+
+
+
 
 // var pool = require('./models/db');
 // pool.query('select * from usuarios').then(function(resultados){
